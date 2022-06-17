01e1645fb9ef9bb5c25a96c58b3511db045d1f13
Kamilla Antunes <kamilla.antunes@mercadolivre.com>
2022-06-17 15:49:02 -0300

    Initial commit

---
+++ Not Bored/Not Bored/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Not Bored
+//
+//  Created by Kamilla Mylena Teixeira Antunes on 17/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
